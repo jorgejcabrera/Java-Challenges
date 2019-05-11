317ac91eefbed1ac346333a95d9d40349a31b962
jorgejcabrera <cabrerajjorge@gmail.com>
2019-05-10 23:14:14 -0300

    tree height

--- src/main/java/com/javachallenges/Java/Challenge/Tree/Tree.java
+++ src/main/java/com/javachallenges/Java/Challenge/Tree/Tree.java
@@ -2,13 +2,59 @@ package com.javachallenges.Java.Challenge.Tree;
 
 public class Tree {
 
+  /**
+   * It must print the values in the tree's preorder traversal as a single line of space-separated
+   * values
+   *
+   * @param node
+   */
   public static void preOrder(Node node) {
-    System.out.print(String.format("%s ",node.data));
+    System.out.print(String.format("%s ", node.data));
     if (node.left != null) {
       preOrder(node.left);
     }
     if (node.right != null) {
       preOrder(node.right);
     }
+  }
+
+  /**
+   * It must print the values in the tree's inorder traversal as a single line of space-separated
+   * values
+   *
+   * @param node
+   */
+  public static void inOrder(Node node) {
+    if (node == null) {
+      return;
+    }
+    inOrder(node.left);
+    System.out.printf("%s ", node.data);
+    inOrder(node.right);
+  }
+
+  /**
+   * @param root of tree
+   * @return The height of a binary tree is the number of edges between the tree's root and its
+   *     furthest leaf
+   */
+  public static int height(Node root) {
+    int left = 0;
+    int right = 0;
+    if (root.left != null) left = height(root.left, left + 1);
+    if (root.right != null) right = height(root.right, right + 1);
+    return left > right ? left : right;
+  }
+
+  private static int height(Node node, int height) {
+    int left = height;
+    int right = height;
+    if (node.left != null) {
+      left = height(node.left, height + 1);
+    }
+    if (node.right != null) {
+      right = height(node.right, height + 1);
+    }
+    return left > right ? left : right;
   }
 }
--- src/test/java/com/javachallenges/Java/Challenge/Tree/TreeTest.java
+++ src/test/java/com/javachallenges/Java/Challenge/Tree/TreeTest.java
@@ -2,6 +2,9 @@ package com.javachallenges.Java.Challenge.Tree;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class TreeTest {
 
   @Test
@@ -21,5 +24,46 @@ public class TreeTest {
 
     // then
     Tree.preOrder(root);
+  }
+
+  @Test
+  public void inOrderShouldWorkOk() {
+    // given
+    Node r4 = new Node(4);
+    Node l3 = new Node(3);
+    l3.right = r4;
+    Node r3 = new Node(6);
+    Node r2 = new Node(5);
+    r2.right = r3;
+    r2.left = l3;
+    Node r1 = new Node(2);
+    r1.right = r2;
+    Node root = new Node(1);
+    root.right = r1;
+
+    // then
+    Tree.inOrder(root);
+  }
+
+  @Test
+  public void heightShouldWorkOk() {
+    // given
+    Node noder31 = new Node(7);
+    Node noder21 = new Node(6);
+    Node nodel22 = new Node(4);
+    Node nodel21= new Node(1);
+    Node noder1 = new Node(5);
+    Node nodel1 = new Node(2);
+    Node root = new Node(3);
+
+    noder21.right = noder31;
+    noder1.right = noder21;
+    noder1.left = nodel22;
+    nodel1.left = nodel21;
+    root.right = noder1;
+    root.left = nodel1;
+
+    // then
+    assertThat(Tree.height(root), equalTo(3));
   }
 }
