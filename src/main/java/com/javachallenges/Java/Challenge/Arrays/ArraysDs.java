5566650c42351901391b431dc10e76b9042623f7
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2021-11-15 21:34:53 -0300

    code format of ArraysDS

--- src/main/java/com/javachallenges/Java/Challenge/Arrays/ArraysDs.java
+++ src/main/java/com/javachallenges/Java/Challenge/Arrays/ArraysDs.java
@@ -9,84 +9,84 @@ import java.util.stream.IntStream;
 
 public class ArraysDs {
 
-  private static Integer queryTypeIndex = 0;
-  private static Integer xIndex = 1;
-  private static Integer yIndex = 2;
+    private static Integer queryTypeIndex = 0;
+    private static Integer xIndex = 1;
+    private static Integer yIndex = 2;
 
-  public static int[] leftRotation(int[] a, int d) {
-    int[] begin = IntStream.range(d, a.length).map(j -> a[j]).toArray();
-    int[] end = IntStream.range(0, d).map(j -> a[j]).toArray();
-    return ArrayUtils.addAll(begin, end);
-  }
-
-  public static int[] reverse(int[] a) {
-    int size = a.length;
-    int elements = size - 1;
-    int[] reverse = new int[size];
-    int i;
-    for (i = 0; i < size; i++) {
-      reverse[i] = a[elements - i];
+    public static int[] leftRotation(int[] a, int d) {
+        int[] begin = Arrays.stream(a, d, a.length).toArray();
+        int[] end = Arrays.stream(a, 0, d).toArray();
+        return ArrayUtils.addAll(begin, end);
     }
-    return reverse;
-  }
 
-  public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
-    int lastAnswer = 0;
-    int index;
-    Integer queryType;
-    Integer x;
-    Integer y;
-    List<List<Integer>> sequences = createSequences(n);
-    List<Integer> output = new ArrayList<>();
-
-    for (List<Integer> query : queries) {
-      queryType = query.get(queryTypeIndex);
-      x = query.get(xIndex);
-      y = query.get(yIndex);
-      index = calculateIndex(x, lastAnswer, n);
-      if (queryType == 1) {
-        sequences.get(index).add(y);
-      } else if (queryType == 2) {
-        int sequenceSize = sequences.get(index).size();
-        lastAnswer = sequences.get(index).get(y % sequenceSize);
-        output.add(lastAnswer);
-        System.out.println(lastAnswer);
-      }
+    public static int[] reverse(int[] a) {
+        int size = a.length;
+        int elements = size - 1;
+        int[] reverse = new int[size];
+        int i;
+        for (i = 0; i < size; i++) {
+            reverse[i] = a[elements - i];
+        }
+        return reverse;
     }
-    return output;
-  }
 
-  private static List<List<Integer>> createSequences(int n) {
-    List<List<Integer>> sequences = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-      sequences.add(new ArrayList<>());
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        int lastAnswer = 0;
+        int index;
+        Integer queryType;
+        Integer x;
+        Integer y;
+        List<List<Integer>> sequences = createSequences(n);
+        List<Integer> output = new ArrayList<>();
+
+        for (List<Integer> query : queries) {
+            queryType = query.get(queryTypeIndex);
+            x = query.get(xIndex);
+            y = query.get(yIndex);
+            index = calculateIndex(x, lastAnswer, n);
+            if (queryType == 1) {
+                sequences.get(index).add(y);
+            } else if (queryType == 2) {
+                int sequenceSize = sequences.get(index).size();
+                lastAnswer = sequences.get(index).get(y % sequenceSize);
+                output.add(lastAnswer);
+                System.out.println(lastAnswer);
+            }
+        }
+        return output;
     }
-    return sequences;
-  }
 
-  public static int[] matchingStrings(String[] strings, String[] queries) {
-    int[] result = new int[queries.length];
-
-    String[] intersection = intersect(strings, queries);
-    for (int i = 0; i < queries.length; i++) {
-      String query = queries[i];
-      int occurrences = 0;
-      for (String string : intersection) {
-        if (query.equals(string)) occurrences = occurrences + 1;
-      }
-      result[i] = occurrences;
+    private static List<List<Integer>> createSequences(int n) {
+        List<List<Integer>> sequences = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            sequences.add(new ArrayList<>());
+        }
+        return sequences;
     }
-    return result;
-  }
 
-  private static String[] intersect(String[] strings, String[] queries) {
-    return Arrays.stream(strings)
-        .map(String::trim)
-        .filter(x -> Arrays.stream(queries).anyMatch(y -> y.equals(x)))
-        .toArray(String[]::new);
-  }
+    public static int[] matchingStrings(String[] strings, String[] queries) {
+        int[] result = new int[queries.length];
 
-  private static int calculateIndex(int value, int lastAnswer, int n) {
-    return (value ^ lastAnswer) % n;
-  }
+        String[] intersection = intersect(strings, queries);
+        for (int i = 0; i < queries.length; i++) {
+            String query = queries[i];
+            int occurrences = 0;
+            for (String string : intersection) {
+                if (query.equals(string)) occurrences = occurrences + 1;
+            }
+            result[i] = occurrences;
+        }
+        return result;
+    }
+
+    private static String[] intersect(String[] strings, String[] queries) {
+        return Arrays.stream(strings)
+                .map(String::trim)
+                .filter(x -> Arrays.asList(queries).contains(x))
+                .toArray(String[]::new);
+    }
+
+    private static int calculateIndex(int value, int lastAnswer, int n) {
+        return (value ^ lastAnswer) % n;
+    }
 }
--- src/test/java/com/javachallenges/Java/Challenge/Arrays/ArraysChallengesTests.java
+++ src/test/java/com/javachallenges/Java/Challenge/Arrays/ArraysChallengesTests.java
@@ -1,6 +1,5 @@
 package com.javachallenges.Java.Challenge.Arrays;
 
-import com.javachallenges.Java.Challenge.Others.MixedChallenges;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -16,196 +15,196 @@ import static org.junit.Assert.assertTrue;
 
 public class ArraysChallengesTests {
 
-  @Test
-  public void challengeArrayOne_findElementInUnsortedIntegerListShouldWorkOk() {
-    // given
-    List<Integer> arr =
-        new ArrayList<Integer>() {
-          {
-            add(1);
-            add(2);
-            add(128458);
-          }
-        };
-    IntStream.rangeClosed(1, 1500)
-        .forEach(i -> arr.add(new Random().ints(1).findFirst().getAsInt()));
-    int size = arr.size();
-    Integer[] sortedArr = arr.toArray(new Integer[size]);
+    @Test
+    public void challengeArrayOne_findElementInUnsortedIntegerListShouldWorkOk() {
+        // given
+        List<Integer> arr =
+                new ArrayList<Integer>() {
+                    {
+                        add(1);
+                        add(2);
+                        add(128458);
+                    }
+                };
+        IntStream.rangeClosed(1, 1500)
+                .forEach(i -> arr.add(new Random().ints(1).findFirst().getAsInt()));
+        int size = arr.size();
+        Integer[] sortedArr = arr.toArray(new Integer[size]);
 
-    // when
-    Arrays.parallelSort(sortedArr);
+        // when
+        Arrays.parallelSort(sortedArr);
 
-    // then
-    assertTrue(Arrays.binarySearch(sortedArr, 1) >= 0);
-    assertTrue(Arrays.binarySearch(sortedArr, 2) >= 0);
-    assertTrue(Arrays.binarySearch(sortedArr, 128458) >= 0);
-  }
+        // then
+        assertTrue(Arrays.binarySearch(sortedArr, 1) >= 0);
+        assertTrue(Arrays.binarySearch(sortedArr, 2) >= 0);
+        assertTrue(Arrays.binarySearch(sortedArr, 128458) >= 0);
+    }
 
-  @Test
-  public void challengeArrayTwo_hourglassSumIn2DArray() {
-    // given
-    int[][] matrix = new int[6][6];
-    matrix[0][0] = -9;
-    matrix[0][1] = -9;
-    matrix[0][2] = -9;
-    matrix[0][3] = 1;
-    matrix[0][4] = 1;
-    matrix[0][5] = 1;
+    @Test
+    public void challengeArrayTwo_hourglassSumIn2DArray() {
+        // given
+        int[][] matrix = new int[6][6];
+        matrix[0][0] = -9;
+        matrix[0][1] = -9;
+        matrix[0][2] = -9;
+        matrix[0][3] = 1;
+        matrix[0][4] = 1;
+        matrix[0][5] = 1;
 
-    matrix[1][1] = -9;
-    matrix[1][3] = 4;
-    matrix[1][4] = 3;
-    matrix[1][5] = 2;
+        matrix[1][1] = -9;
+        matrix[1][3] = 4;
+        matrix[1][4] = 3;
+        matrix[1][5] = 2;
 
-    matrix[2][0] = -9;
-    matrix[2][1] = -9;
-    matrix[2][2] = -9;
-    matrix[2][3] = 1;
-    matrix[2][4] = 2;
-    matrix[2][5] = 3;
+        matrix[2][0] = -9;
+        matrix[2][1] = -9;
+        matrix[2][2] = -9;
+        matrix[2][3] = 1;
+        matrix[2][4] = 2;
+        matrix[2][5] = 3;
 
-    matrix[3][2] = 8;
-    matrix[3][3] = 6;
-    matrix[3][4] = 6;
+        matrix[3][2] = 8;
+        matrix[3][3] = 6;
+        matrix[3][4] = 6;
 
-    matrix[4][3] = -2;
+        matrix[4][3] = -2;
 
-    matrix[5][2] = 1;
-    matrix[5][3] = 2;
-    matrix[5][4] = 4;
+        matrix[5][2] = 1;
+        matrix[5][3] = 2;
+        matrix[5][4] = 4;
 
-    // when
-    List<int[][]> vertexes = HourGlass.getVertexesList(matrix);
-    Integer firstVertexSum = HourGlass.sum(matrix, vertexes.get(0));
-    Integer secondVertexSum = HourGlass.sum(matrix, vertexes.get(1));
-    Integer maxSum = HourGlass.hourglassSum(matrix);
+        // when
+        List<int[][]> vertexes = HourGlass.getVertexesList(matrix);
+        Integer firstVertexSum = HourGlass.sum(matrix, vertexes.get(0));
+        Integer secondVertexSum = HourGlass.sum(matrix, vertexes.get(1));
+        Integer maxSum = HourGlass.hourglassSum(matrix);
 
-    // then
-    assertThat(vertexes.size(), equalTo(16));
-    assertThat(firstVertexSum, equalTo(-63));
-    assertThat(secondVertexSum, equalTo(-34));
-    assertThat(maxSum, equalTo(28));
-  }
+        // then
+        assertThat(vertexes.size(), equalTo(16));
+        assertThat(firstVertexSum, equalTo(-63));
+        assertThat(secondVertexSum, equalTo(-34));
+        assertThat(maxSum, equalTo(28));
+    }
 
-  @Test
-  public void challengeArrayTwo_withCompleteMatrixShouldWorkOk() {
-    // given
-    int[][] matrix = new int[6][6];
-    matrix[0][0] = -1;
-    matrix[0][1] = -1;
-    matrix[0][2] = 0;
-    matrix[0][3] = -9;
-    matrix[0][4] = -2;
-    matrix[0][5] = -2;
+    @Test
+    public void challengeArrayTwo_withCompleteMatrixShouldWorkOk() {
+        // given
+        int[][] matrix = new int[6][6];
+        matrix[0][0] = -1;
+        matrix[0][1] = -1;
+        matrix[0][2] = 0;
+        matrix[0][3] = -9;
+        matrix[0][4] = -2;
+        matrix[0][5] = -2;
 
-    matrix[1][0] = -2;
-    matrix[1][1] = -1;
-    matrix[1][2] = -6;
-    matrix[1][3] = -8;
-    matrix[1][4] = -2;
-    matrix[1][5] = -5;
+        matrix[1][0] = -2;
+        matrix[1][1] = -1;
+        matrix[1][2] = -6;
+        matrix[1][3] = -8;
+        matrix[1][4] = -2;
+        matrix[1][5] = -5;
 
-    matrix[2][0] = -1;
-    matrix[2][1] = -1;
-    matrix[2][2] = -1;
-    matrix[2][3] = -2;
-    matrix[2][4] = -3;
-    matrix[2][5] = -4;
+        matrix[2][0] = -1;
+        matrix[2][1] = -1;
+        matrix[2][2] = -1;
+        matrix[2][3] = -2;
+        matrix[2][4] = -3;
+        matrix[2][5] = -4;
 
-    matrix[3][0] = -1;
-    matrix[3][1] = -9;
-    matrix[3][2] = -2;
-    matrix[3][3] = -4;
-    matrix[3][4] = -4;
-    matrix[3][5] = -5;
+        matrix[3][0] = -1;
+        matrix[3][1] = -9;
+        matrix[3][2] = -2;
+        matrix[3][3] = -4;
+        matrix[3][4] = -4;
+        matrix[3][5] = -5;
 
-    matrix[4][0] = -7;
-    matrix[4][1] = -3;
-    matrix[4][2] = -3;
-    matrix[4][3] = -2;
-    matrix[4][4] = -9;
-    matrix[4][5] = -9;
+        matrix[4][0] = -7;
+        matrix[4][1] = -3;
+        matrix[4][2] = -3;
+        matrix[4][3] = -2;
+        matrix[4][4] = -9;
+        matrix[4][5] = -9;
 
-    matrix[5][0] = -1;
-    matrix[5][1] = -3;
-    matrix[5][2] = -1;
-    matrix[5][3] = -2;
-    matrix[5][4] = -4;
-    matrix[5][5] = -5;
+        matrix[5][0] = -1;
+        matrix[5][1] = -3;
+        matrix[5][2] = -1;
+        matrix[5][3] = -2;
+        matrix[5][4] = -4;
+        matrix[5][5] = -5;
 
-    // when
-    List<int[][]> vertexes = HourGlass.getVertexesList(matrix);
-    Integer firstVertexSum = HourGlass.sum(matrix, vertexes.get(0));
-    Integer secondVertexSum = HourGlass.sum(matrix, vertexes.get(1));
-    Integer maxSum = HourGlass.hourglassSum(matrix);
+        // when
+        List<int[][]> vertexes = HourGlass.getVertexesList(matrix);
+        Integer firstVertexSum = HourGlass.sum(matrix, vertexes.get(0));
+        Integer secondVertexSum = HourGlass.sum(matrix, vertexes.get(1));
+        Integer maxSum = HourGlass.hourglassSum(matrix);
 
-    // then
-    assertThat(vertexes.size(), equalTo(16));
-    assertThat(firstVertexSum, equalTo(-6));
-    assertThat(secondVertexSum, equalTo(-20));
-    assertThat(maxSum, equalTo(-6));
-  }
+        // then
+        assertThat(vertexes.size(), equalTo(16));
+        assertThat(firstVertexSum, equalTo(-6));
+        assertThat(secondVertexSum, equalTo(-20));
+        assertThat(maxSum, equalTo(-6));
+    }
 
-  @Test
-  public void challengeArrayThreeReverseArrayShouldWorkOk() {
-    // given
-    int[] a = new int[] {1, 4, 3, 2};
+    @Test
+    public void challengeArrayThreeReverseArrayShouldWorkOk() {
+        // given
+        int[] a = new int[]{1, 4, 3, 2};
 
-    // when
-    int[] reversedArray = ArraysDs.reverse(a);
+        // when
+        int[] reversedArray = ArraysDs.reverse(a);
 
-    // then
-    assertThat(reversedArray[0], equalTo(2));
-    assertThat(reversedArray[1], equalTo(3));
-    assertThat(reversedArray[2], equalTo(4));
-    assertThat(reversedArray[3], equalTo(1));
-  }
+        // then
+        assertThat(reversedArray[0], equalTo(2));
+        assertThat(reversedArray[1], equalTo(3));
+        assertThat(reversedArray[2], equalTo(4));
+        assertThat(reversedArray[3], equalTo(1));
+    }
 
-  @Test
-  public void challengeArrayFourDynamicArrayShouldWorkOk() {
-    // given
-    int numberOfSequences = 2;
-    List<Integer> firstQuery = Arrays.asList(1, 0, 5);
-    List<Integer> secondQuery = Arrays.asList(1, 1, 7);
-    List<Integer> thirdQuery = Arrays.asList(1, 0, 3);
-    List<Integer> fourthQuery = Arrays.asList(2, 1, 0);
-    List<Integer> fifthQuery = Arrays.asList(2, 1, 1);
-    List<List<Integer>> queries =
-        Arrays.asList(firstQuery, secondQuery, thirdQuery, fourthQuery, fifthQuery);
+    @Test
+    public void challengeArrayFourDynamicArrayShouldWorkOk() {
+        // given
+        int numberOfSequences = 2;
+        List<Integer> firstQuery = Arrays.asList(1, 0, 5);
+        List<Integer> secondQuery = Arrays.asList(1, 1, 7);
+        List<Integer> thirdQuery = Arrays.asList(1, 0, 3);
+        List<Integer> fourthQuery = Arrays.asList(2, 1, 0);
+        List<Integer> fifthQuery = Arrays.asList(2, 1, 1);
+        List<List<Integer>> queries =
+                Arrays.asList(firstQuery, secondQuery, thirdQuery, fourthQuery, fifthQuery);
 
-    // when
-    List<Integer> dynamicArray = ArraysDs.dynamicArray(numberOfSequences, queries);
+        // when
+        List<Integer> dynamicArray = ArraysDs.dynamicArray(numberOfSequences, queries);
 
-    // then
-    assertThat(dynamicArray.contains(7), is(true));
-    assertThat(dynamicArray.contains(3), is(true));
-  }
+        // then
+        assertThat(dynamicArray.contains(7), is(true));
+        assertThat(dynamicArray.contains(3), is(true));
+    }
 
-  @Test
-  public void challengeFiveReverseArrayShouldWorkOk() {
-    // given
-    int[] array = {1, 2, 3, 4, 5};
-    int d = 4;
+    @Test
+    public void challengeFiveReverseArrayShouldWorkOk() {
+        // given
+        int[] array = {1, 2, 3, 4, 5};
+        int d = 4;
 
-    // when
-    int[] result = ArraysDs.leftRotation(array, d);
+        // when
+        int[] result = ArraysDs.leftRotation(array, d);
 
-    // then
-    assertThat(result[0], equalTo(5));
-    assertThat(result[1], equalTo(1));
-    assertThat(result[4], equalTo(4));
-  }
+        // then
+        assertThat(result[0], equalTo(5));
+        assertThat(result[1], equalTo(1));
+        assertThat(result[4], equalTo(4));
+    }
 
-  @Test
-  public void challengeMatchingStrings() {
-    // given
-    String[] strings = {"aba", "baba", " aba", "xzxb"};
-    String[] queries = {"aba", "xzxb", "ab"};
+    @Test
+    public void challengeMatchingStrings() {
+        // given
+        String[] strings = {"aba", "baba", " aba", "xzxb"};
+        String[] queries = {"aba", "xzxb", "ab"};
 
-    // when
-    int[] results = ArraysDs.matchingStrings(strings, queries);
+        // when
+        int[] results = ArraysDs.matchingStrings(strings, queries);
 
-    // then
-    assertThat(results[0], equalTo(2));
-  }
+        // then
+        assertThat(results[0], equalTo(2));
+    }
 }
